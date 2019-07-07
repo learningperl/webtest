fa475c6d1808dddf0e544b54a5515af7a7f033db
Will <1052949192@qq.com>
2019-07-07 21:11:56 +0800

    完成文件上传开发
    Signed-off-by: Will <1052949192@qq.com>

--- runner.py
+++ runner.py
@@ -5,4 +5,7 @@ from web.webcase import Web
 # 打开浏览器
 web = Web()
 
+#上传文件
+web.upload()
+
 web.close()
--- web/webcase.py
+++ web/webcase.py
@@ -9,6 +9,14 @@ class Web:
         #打开浏览器
         self.drvier = webdriver.Chrome(executable_path='lib/chromedriver.exe')
 
+    #上传文件
+    def upload(self):
+        self.drvier.implicitly_wait(20)
+        self.drvier.get('https://www.baidu.com/')
+        self.drvier.find_element_by_xpath('//*[@id="form"]/span[1]/span').click()
+        self.drvier.find_element_by_xpath('//*[@id="form"]/div/div[2]/div[2]/input').send_keys('F:/git/webtest/lib/1.png')
+        time.sleep(10)
+
 
     def close(self):
         # 等待3秒然后退出
